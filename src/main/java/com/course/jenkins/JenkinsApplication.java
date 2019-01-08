4d27dbc9304f468c64bc2380a942094f38c82be1
Paulo Sergio Pereira Junior <paulosergio.jnr@gmail.com>
2019-01-08 07:00:59 -0200

    first commit

---
+++ src/main/java/com/course/jenkins/JenkinsApplication.java
@@ -0,0 +1,16 @@
+package com.course.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+		
+		System.out.println("This is a test!");
+	}
+
+}
+
